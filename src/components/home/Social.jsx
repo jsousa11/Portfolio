fdcfc61cfc1d8458964d71fdbfa36152876e72b2
João Sousa <75133059+jsousa11@users.noreply.github.com>
2024-09-11 20:56:04 +0100

    Update Social.jsx

--- src/components/home/Social.jsx
+++ src/components/home/Social.jsx
@@ -3,11 +3,11 @@ import React from "react";
 const Social = () => {
     return (
         <div className="home__social">
-            <a href="https://www.linkedin.com/in/jsousa11/" className="home__social-icon" target="_blank">
+            <a href="https://www.linkedin.com/in/jsousa11/" className="home__social-icon" target="_blank" rel="noopener noreferrer">
                 <i className="uil uil-linkedin-alt"></i>
             </a>
 
-            <a href="https://github.com/jsousa11" className="home__social-icon" target="_blank">
+            <a href="https://github.com/jsousa11" className="home__social-icon" target="_blank" rel="noopener noreferrer">
                 <i className="uil uil-github-alt"></i>
             </a>
         </div>
